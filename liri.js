907c6eec3c27b3530fc1fe282af40b5579aee28b
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-08-19 11:30:59 -0700

    Added more variables

--- liri.js
+++ liri.js
@@ -20,11 +20,18 @@ var spotify = new spotify({
 
 var OMDB = require("omdb-client");
 var omdbKEYS = keys.omdb;
+omdbAPIkey = omdbKEYS.id;
 
 
 var input = process.argv[2]
 var content = process.argv[3]
 
+var chalk = require('chalk');
+var LiriTitle = chalk.bgYellow
+var liriAnswer = LiriTitle("\n-----HERE IS WHAT THE ROBIT FOUND!-----\n")
+var liriProblem = LiriTitle("\n-----DANGER! DANGER WILL ROBINSON-----\n")
+
+//Here be the non-global code//
 
 
 
