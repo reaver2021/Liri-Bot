a9cd8ed1b362874a34f4072e3be9841a45eda69f
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-08-19 12:12:09 -0700

    changed global

--- liri.js
+++ liri.js
@@ -4,34 +4,20 @@ var keys = require("./keys.js");
 var fs = require('fs');
 var moment = require('moment');
 var inquirer = require('inquirer');
-
 var axios = require('axios');
-var bandsinTown = keys.bandsInTown;
-var bandsINtown = bandsinTown.id;
 
-var Spotify = require('spotify')
-var spotifyKEYS = keys.spotify;
-var spotifyID = spotifyKEYS.id;
-var secretSpotify = spotifyKEYS.secret;
-var spotify = new spotify({
-    id:spotifyID,
-    secret: secretSpotify
-});
+var spotify = new spotify(keys.spotify)
 
-var OMDB = require("omdb-client");
-var omdbKEYS = keys.omdb;
-omdbAPIkey = omdbKEYS.id;
+var action = process.argv[2];
+var choice = process.argv[3];
 
+function switchCase(){
+    switch(action) {
+        
+        case 'concert-this':
+        bandsInTown(chocie)
 
-var input = process.argv[2]
-var content = process.argv[3]
-
-var chalk = require('chalk');
-var LiriTitle = chalk.bgYellow
-var liriAnswer = LiriTitle("\n-----HERE IS WHAT THE ROBIT FOUND!-----\n")
-var liriProblem = LiriTitle("\n-----DANGER! DANGER WILL ROBINSON-----\n")
-
-//Here be the non-global code//
-
-
-
+        case 'spotify-this-song':
+            sp
+    }
+}
