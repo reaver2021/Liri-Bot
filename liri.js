8b0e651ff2ce208a0a2620214cb28446f5ded03e
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-08-19 11:15:16 -0700

    added vars

--- liri.js
+++ liri.js
@@ -1,5 +1,30 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var fs = require('fs');
+var moment = require('moment');
+var inquirer = require('inquirer');
 
-var spotify = new spotify(keys.spotify);
+var axios = require('axios');
+var bandsinTown = keys.bandsInTown;
+var bandsINtown = bandsinTown.id;
+
+var Spotify = require('spotify')
+var spotifyKEYS = keys.spotify;
+var spotifyID = spotifyKEYS.id;
+var secretSpotify = spotifyKEYS.secret;
+var spotify = new spotify({
+    id:spotifyID,
+    secret: secretSpotify
+});
+
+var OMDB = require("omdb-client");
+var omdbKEYS = keys.omdb;
+
+
+var input = process.argv[2]
+var content = process.argv[3]
+
+
+
+
