0f591361b98d00821eb67f5fb1c0cd4488a5e493
Griffin Sprague <griffin.spraguedixon23@gmail.com>
2019-08-21 09:28:37 -0700

    Added OMDB

--- liri.js
+++ liri.js
@@ -1,23 +1,35 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var fs = require('fs');
-var moment = require('moment');
-var inquirer = require('inquirer');
-var axios = require('axios');
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment")
+var fs = require("fs");
 
-var spotify = new spotify(keys.spotify)
+var action = process.argv[2]
+var parameter = process.argv.splice(3).join(" ");
 
-var action = process.argv[2];
-var choice = process.argv[3];
+if (action === "movie-this" && parameter) {
 
-function switchCase(){
-    switch(action) {
-        
-        case 'concert-this':
-        bandsInTown(chocie)
+    axios.get("http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy")
+        .then(
+            function(response) {
+                console.log("Do you not watch movies? What is wrong with you? Anyway here's Mr.Nobody")
+                console.log("Title: " + response.data.Title);
+                console.log("Year Released: " + response.data.Year);
+                console.log("iMDB Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Score: " + response.data.Ratings[1].value);
+                console.log("Filmed In: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+            })
+        .catch(function(error){
+            if(error.response){
+                
+            }
+        })
+            
 
-        case 'spotify-this-song':
-            sp
-    }
-}
+
+
